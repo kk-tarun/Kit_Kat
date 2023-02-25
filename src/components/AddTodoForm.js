4f71fffc9e74d1a68740724dc344c7f2b54eeec9
Vishnu Prakash <vishnuash1717@gmail.com>
2023-02-25 09:24:48 +0530

    added modal

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,5 +1,125 @@
+// import React, { useState, useRef, useEffect } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import '../style/AddTodoForm.css';
+
+// function AddTodoForm({ onAddTodo }) {
+//   const [newTodo, setNewTodo] = useState({
+//     id: "",
+//     name: "",
+//     date: new Date().toISOString().substring(0, 10),
+//     type: "",
+//     status: "",
+//   });
+//   const dateInputRef = useRef();
+
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+//     setNewTodo({
+//       ...newTodo,
+//       [name]: value,
+//     });
+//   }
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     const todoWithId = {
+//       ...newTodo,
+//       id: uuidv4(),
+//     };
+//     onAddTodo && onAddTodo(todoWithId);
+//     setNewTodo({
+//       id: "",
+//       name: "",
+//       date: "",
+//       type: "",
+//       status: "",
+//     });
+//     dateInputRef.current.value = "";
+//   }
+
+//   useEffect(() => {
+//     dateInputRef.current.value = newTodo.date;
+//   });
+
+//   return (
+//     <div>
+//       <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+//       style={{float:"right"}}>
+//         Add
+//       </button>
+
+//       <div className="modal fade text-dark container-fluid" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//         <div className="modal-dialog modal-dialog-centered modal-lg">
+//           <div className="modal-content">
+//             <button type="button" className="btn-close" style={{float:"right"}} data-bs-dismiss="modal" aria-label="Close"></button>
+//             <div className="modal-body">
+//               <form onSubmit={handleSubmit}>
+//                 <div className="row">
+//                   <div className="col">
+//                     <label htmlFor="name">Name:</label>
+//                     <input
+//                       type="text"
+//                       id="name"
+//                       name="name"
+//                       value={newTodo.name}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </div>
+
+//                   <div className="col">
+//                     <label htmlFor="date">Date:</label>
+//                     <input
+//                       type="date"
+//                       id="date"
+//                       name="date"
+//                       ref={dateInputRef}
+//                       value={newTodo.date}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </div>
+//                 </div>
+
+//                 <div className="row">
+//                   <div className="col">
+//                     <label htmlFor="type">Type:</label>
+//                     <input
+//                       type="text"
+//                       id="type"
+//                       name="type"
+//                       value={newTodo.type}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </div>
+//                   <div className="col">
+//                     <label htmlFor="status">Status:</label>
+//                     <input
+//                       type="text"
+//                       id="status"
+//                       name="status"
+//                       value={newTodo.status}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </div>
+//                 </div>
+//                 <button className="btn btn-primary" type="submit">Add Todo</button>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default AddTodoForm;
+// ``tarun
 import React, { useState, useRef, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid"; 
+import { v4 as uuidv4 } from "uuid";
+// import '../style/AddTodoForm.css';
 
 function AddTodoForm({ onAddTodo }) {
   const [newTodo, setNewTodo] = useState({
@@ -33,54 +153,118 @@ function AddTodoForm({ onAddTodo }) {
       type: "",
       status: "",
     });
-    dateInputRef.current.value = ""; 
+    dateInputRef.current.value = "";
   }
 
   useEffect(() => {
     dateInputRef.current.value = newTodo.date;
-  }, []);
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={newTodo.name}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="date">Date:</label>
-      <input
-        type="date"
-        id="date"
-        name="date"
-        ref={dateInputRef}
-        value={newTodo.date}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="type">Type:</label>
-      <input
-        type="text"
-        id="type"
-        name="type"
-        value={newTodo.type}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="status">Status:</label>
-      <input
-        type="text"
-        id="status"
-        name="status"
-        value={newTodo.status}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Add Todo</button>
-    </form>
+    <div>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        style={{ float: "right" }}
+      >
+        Add
+      </button>
+
+      <div
+        className="modal fade text-dark container-fluid"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div
+          className="modal-dialog modal-dialog-centered modal-lg"
+          style={{ minHeight: "100vh" }}
+        >
+          <div className="modal-content">
+            <button
+              type="button"
+              className="btn-close"
+              style={{ float: "right" }}
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={newTodo.name}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+
+                  <div className="col">
+                    <label htmlFor="date">Date:</label>
+                    <input
+                      type="date"
+                      id="date"
+                      name="date"
+                      ref={dateInputRef}
+                      value={newTodo.date}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col">
+                    <label htmlFor="type">Type:</label>
+                    {/* <input
+                      type="text"
+                      id="type"
+                      name="type"
+                      value={newTodo.type}
+                      onChange={handleChange}
+                      required
+                    /> */}
+                    <select
+                      id="type"
+                      name="type"
+                      value={newTodo.type}
+                      onChange={handleChange}
+                      required
+                    >
+                      <option value="" hidden>--Select Type--</option>
+                      <option value="Project">Project</option>
+                      <option value="Interview">Interview</option>
+                      <option value="Learning">Learning</option>
+                    </select>
+                  </div>
+                  <div className="col">
+                    <label htmlFor="status">Status:</label>
+                    <input
+                      type="text"
+                      id="status"
+                      name="status"
+                      value={newTodo.status}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <button className="btn btn-primary" type="submit" data-bs-toggle="modal">
+                  Add Todo
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
