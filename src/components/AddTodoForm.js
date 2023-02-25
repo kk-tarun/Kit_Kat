32bf2104bab8d1f76a86d74e94418df0ed2273eb
Tarun Kumar Kushwaha <74929799+kk-tarun@users.noreply.github.com>
2023-02-25 11:19:13 +0530

    add buttons

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,127 +1,8 @@
-// import React, { useState, useRef, useEffect } from "react";
-// import { v4 as uuidv4 } from "uuid";
-// import '../style/AddTodoForm.css';
-
-// function AddTodoForm({ onAddTodo }) {
-//   const [newTodo, setNewTodo] = useState({
-//     id: "",
-//     name: "",
-//     date: new Date().toISOString().substring(0, 10),
-//     type: "",
-//     status: "",
-//   });
-//   const dateInputRef = useRef();
-
-//   function handleChange(event) {
-//     const { name, value } = event.target;
-//     setNewTodo({
-//       ...newTodo,
-//       [name]: value,
-//     });
-//   }
-
-//   function handleSubmit(event) {
-//     event.preventDefault();
-//     const todoWithId = {
-//       ...newTodo,
-//       id: uuidv4(),
-//     };
-//     onAddTodo && onAddTodo(todoWithId);
-//     setNewTodo({
-//       id: "",
-//       name: "",
-//       date: "",
-//       type: "",
-//       status: "",
-//     });
-//     dateInputRef.current.value = "";
-//   }
-
-//   useEffect(() => {
-//     dateInputRef.current.value = newTodo.date;
-//   });
-
-//   return (
-//     <div>
-//       <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
-//       style={{float:"right"}}>
-//         Add
-//       </button>
-
-//       <div className="modal fade text-dark container-fluid" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//         <div className="modal-dialog modal-dialog-centered modal-lg">
-//           <div className="modal-content">
-//             <button type="button" className="btn-close" style={{float:"right"}} data-bs-dismiss="modal" aria-label="Close"></button>
-//             <div className="modal-body">
-//               <form onSubmit={handleSubmit}>
-//                 <div className="row">
-//                   <div className="col">
-//                     <label htmlFor="name">Name:</label>
-//                     <input
-//                       type="text"
-//                       id="name"
-//                       name="name"
-//                       value={newTodo.name}
-//                       onChange={handleChange}
-//                       required
-//                     />
-//                   </div>
-
-//                   <div className="col">
-//                     <label htmlFor="date">Date:</label>
-//                     <input
-//                       type="date"
-//                       id="date"
-//                       name="date"
-//                       ref={dateInputRef}
-//                       value={newTodo.date}
-//                       onChange={handleChange}
-//                       required
-//                     />
-//                   </div>
-//                 </div>
-
-//                 <div className="row">
-//                   <div className="col">
-//                     <label htmlFor="type">Type:</label>
-//                     <input
-//                       type="text"
-//                       id="type"
-//                       name="type"
-//                       value={newTodo.type}
-//                       onChange={handleChange}
-//                       required
-//                     />
-//                   </div>
-//                   <div className="col">
-//                     <label htmlFor="status">Status:</label>
-//                     <input
-//                       type="text"
-//                       id="status"
-//                       name="status"
-//                       value={newTodo.status}
-//                       onChange={handleChange}
-//                       required
-//                     />
-//                   </div>
-//                 </div>
-//                 <button className="btn btn-primary" type="submit">Add Todo</button>
-//               </form>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AddTodoForm;
-// ``tarun
 import React, { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-// import '../style/AddTodoForm.css';
+import '../style/AddTodoForm.css';
 
-function AddTodoForm({ onAddTodo }) {
+function AddTodoForm({ onAddTodo } ) {
   const [newTodo, setNewTodo] = useState({
     id: "",
     name: "",
@@ -130,6 +11,7 @@ function AddTodoForm({ onAddTodo }) {
     status: "",
   });
   const dateInputRef = useRef();
+  const [count, setCount] = useState(0);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -154,6 +36,7 @@ function AddTodoForm({ onAddTodo }) {
       status: "",
     });
     dateInputRef.current.value = "";
+    setCount(count+1)
   }
 
   useEffect(() => {
@@ -162,18 +45,18 @@ function AddTodoForm({ onAddTodo }) {
 
   return (
     <div>
-      <button
+      {count !== 0 && <button
         type="button"
         className="btn btn-primary"
         data-bs-toggle="modal"
         data-bs-target="#exampleModal"
-        style={{ float: "right" }}
+        style={{ float: "right"}}
       >
         Add
-      </button>
+      </button>}
 
       <div
-        className="modal fade text-dark container-fluid"
+        className="modal fade text-dark"
         id="exampleModal"
         tabIndex="-1"
         aria-labelledby="exampleModalLabel"
@@ -183,11 +66,12 @@ function AddTodoForm({ onAddTodo }) {
           className="modal-dialog modal-dialog-centered modal-lg"
           style={{ minHeight: "100vh" }}
         >
-          <div className="modal-content">
+          <div className="modal-content container-fluid">
             <button
               type="button"
               className="btn-close"
-              style={{ float: "right" }}
+              style={{ float: "right", alignSelf:'flex-end',
+            margin:'5px' }}
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
@@ -223,14 +107,6 @@ function AddTodoForm({ onAddTodo }) {
                 <div className="row">
                   <div className="col">
                     <label htmlFor="type">Type:</label>
-                    {/* <input
-                      type="text"
-                      id="type"
-                      name="type"
-                      value={newTodo.type}
-                      onChange={handleChange}
-                      required
-                    /> */}
                     <select
                       id="type"
                       name="type"
@@ -256,7 +132,7 @@ function AddTodoForm({ onAddTodo }) {
                     />
                   </div>
                 </div>
-                <button className="btn btn-primary" type="submit" data-bs-toggle="modal">
+                <button className="btn btn-primary" type="submit" data-bs-toggle="modal" style={{marginTop:'40px'}}>
                   Add Todo
                 </button>
               </form>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import TodoItem from "./TodoItem";
-import data from "../data/todos.json";
 import "../style/TodoList.css";
 import AddTodoForm from "./AddTodoForm";
 
 function TodoList() {
-  const [todos, setTodos] = useState([{"id":1,"name":"Marcie","date":"17/11/2022","type":"solo","status":"Completed"}]);
+  const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState(0);
 
   function handleAddTodo(newTodo) {
     setTodos([...todos, newTodo]);
+    setCount(count+1);
   }
 
   function handleDeleteTodo(todoId) {
     setTodos(todos.filter((todo) => todo.id !== todoId));
+    setCount(count-1)
   }
 
   return (
@@ -30,15 +32,35 @@ function TodoList() {
         </thead>
         <tbody className="body">
           {todos !== [] &&
-          todos.map((todo,index) => (
-            <TodoItem
-              key={index}
-              todo={todo}
-              onDeleteTodo={handleDeleteTodo}
-            />
-          ))}
+            todos.map((todo, index) => (
+              <TodoItem
+                key={index}
+                todo={todo}
+                onDeleteTodo={handleDeleteTodo}
+              />
+            ))}
         </tbody>
       </table>
+      {todos.length === 0 &&
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          style={{
+            margin: '40px auto',
+            width: '100%',
+            height: '500px',
+            border: '2px dashed #c0c0c0',
+            backgroundColor: 'transparent'
+          }}
+        >
+          <i class="fa-solid fa-file-pen" style={{ fontSize: '100px' }}></i>
+          <br />
+          <br />
+          <span style={{ marginLeft: '-25px' }}>Click to add Task</span>
+        </button>
+      }
     </div>
   );
 }
